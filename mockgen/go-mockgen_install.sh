492b1de73eb6d799146af98f105c201d7921ea11
wangbingyao <wangbingyao@guazi.com>
2018-04-20 13:00:42 +0800

    fix

--- mockgen/go-mockgen_install.sh
+++ mockgen/go-mockgen_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf $GOPATH/github.com/golang/mock && go get -u github.com/Guazi-inc/mock/mockgen && curl https://raw.githubusercontent.com/Guazi-inc/mock/master/mockgen/go-mockgen.sh > /usr/local/bin/go-mockgen && chmod +x /usr/local/bin/go-mockgen || exit 1
+rm -rf $GOPATH/src/github.com/golang/mock && go get -u github.com/Guazi-inc/mock/mockgen && curl https://raw.githubusercontent.com/Guazi-inc/mock/master/mockgen/go-mockgen.sh > /usr/local/bin/go-mockgen && chmod +x /usr/local/bin/go-mockgen || exit 1
 
 echo "Install successfully"
