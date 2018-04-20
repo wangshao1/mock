dc8c189693e48d8072cd198a3475471f88bdd2ce
wangbingyao <wangbingyao@guazi.com>
2018-04-20 12:44:20 +0800

    exit 1

--- mockgen/go-mockgen.sh
+++ mockgen/go-mockgen.sh
@@ -24,12 +24,13 @@ arr=(${paths//,/ })
 for path in ${arr[*]}
 do
     echo "Generating Mock For: $path"
-    rm -rf $path/mocks && mkdir $path/mocks
+    rm -rf $path/mocks && mkdir $path/mocks || exit 1
     for gofile in `find $path -path $path/mocks/* -prune -o -path $path/vender/* -prune -o -type f -name "*.go" | grep -v "_test.go"`
     do
 #      echo "$path/mocks/${gofile#*$path}"
       dir=`dirname $path/mocks/${gofile#*$path}`
-      if [ ! -d $dir ]; then mkdir -p $dir; fi && touch $path/mocks/${gofile#*$path} && mockgen -source=$gofile -destination $path/mocks/${gofile#*$path} -package `basename $dir`
+      if [ ! -d $dir ]; then mkdir -p $dir; fi && touch $path/mocks/${gofile#*$path} && mockgen -source=$gofile -destination $path/mocks/${gofile#*$path} -package `basename $dir` || exit 1
     done
 done
 
+exit 0
--- mockgen/go-mockgen_install.sh
+++ mockgen/go-mockgen_install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf $GOPATH/github.com/golang/mock
-go get -u github.com/Guazi-inc/mock/mockgen
-curl https://raw.githubusercontent.com/Guazi-inc/mock/master/mockgen/go-mockgen.sh > /usr/local/bin/go-mockgen
-chmod +x /usr/local/bin/go-mockgen
+rm -rf $GOPATH/github.com/golang/mock && go get -u github.com/Guazi-inc/mock/mockgen && curl https://raw.githubusercontent.com/Guazi-inc/mock/master/mockgen/go-mockgen.sh > /usr/local/bin/go-mockgen && chmod +x /usr/local/bin/go-mockgen || exit 1
+
 echo "Install successfully"
