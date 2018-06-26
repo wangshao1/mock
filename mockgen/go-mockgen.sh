1d5942450fa943112b4c0ac4b51d857b7bc6ae92
wangbingyao <wangbingyao@guazi.com>
2018-06-26 10:12:40 +0800

    fix

--- mockgen/go-mockgen.sh
+++ mockgen/go-mockgen.sh
@@ -25,7 +25,7 @@ for path in ${arr[*]}
 do
     echo "Generating Mock For: $path"
     rm -rf $path/mocks && mkdir $path/mocks || exit 1
-    for gofile in `find $path -path $path/mocks/* -prune -o -path $path/vendor/* -prune -o -type f -name "*.go" | grep -v "_test.go"`
+    for gofile in `find $path -path $path/mocks -prune -o -path $path/vendor -prune -o -type f -name "*.go" | grep ".go" | grep -v "_test.go"`
     do
 #      echo "$path/mocks/${gofile#*$path}"
       dir=`dirname $path/mocks/${gofile#*$path}`
