0ea032ae28a3819116540885bd1133dbd8b4cc84
wangshaoyi <wangshaoyi@guazi.com>
2018-08-23 18:07:26 +0800

    修改go-mockgen.sh

--- mockgen/go-mockgen.sh
+++ mockgen/go-mockgen.sh
@@ -25,11 +25,15 @@ for path in ${arr[*]}
 do
     echo "Generating Mock For: $path"
     rm -rf $path/mocks && mkdir $path/mocks || exit 1
-    for gofile in `find $path -path $path//mocks -prune -o -path $path/vendor -prune -o -type f -name "*.go" | grep ".go" | grep -v "_test.go"`
+    for gofile in `find $path -type f -name "*.go" | grep ".go" | grep -v "_test.go"`
+    #for gofile in `find $path -path $path/mocks -prune -o -type f -name "*.go" | grep ".go" | grep -v "_test.go"`#-path $path/vendor -prune -o 
     do
-#      echo "$path/mocks/${gofile#*$path}"
-      dir=`dirname $path/mocks/${gofile#*$path}`
-      if [ ! -d $dir ]; then mkdir -p $dir; fi && touch $path/mocks/${gofile#*$path} && mockgen -source=$gofile -destination $path/mocks/${gofile#*$path} -package `basename $dir` || exit 1
+        if [ -f $gofile ]; then 
+            dir=`dirname $path/mocks${gofile#*$path}`
+            if [ ! -d $dir ]; then mkdir -p $dir; fi && \
+            touch $path/mocks/${gofile#*$path} && \
+            mockgen -source=$gofile -destination $path/mocks/${gofile#*$path} -package `basename $dir` fi || exit 1;
+        fi
     done
 done
 
